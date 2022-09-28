dbb241781e38ecf83477034bef7d4b09e66af718
dre7283 <Dre7283@gmail.com>
2022-09-28 18:04:15 -0400

    updated dashboard routes

---
+++ Main/controllers/dashboard-routes.js
@@ -0,0 +1,43 @@
+const router = require('express').Router();
+const { Post } = require('../models/');
+const withAuth = require('../utils/auth');
+
+router.get('/', withAuth, async (req, res) => {
+    try {
+        const postData = await Post.findAll({
+            where: {
+                userId: req.session.userId,
+            },
+        });
+
+        const posts = postData.map((post) => post.get({plain: true}));
+
+        res.render('all-posts-admin', {
+            layout: 'dashboard',
+            posts,
+        });
+    } catch (err) {
+        res.redirect('login');
+    }
+});
+
+router.get('/edit/:id', withAuth, async (req, res) => {
+    try{
+        const postData = await Post.findByPk(req.params.id);
+
+        if(postData) {
+            const post = postData.get({ plain: true});
+            
+                res.render('edit-post', {
+                    layout: 'dashboard',
+                    post,
+                });
+        } else {
+            res.status(404).end();
+        }
+    } catch (err) {
+        res.redirect('login');
+    }
+});
+
+module.exports = router;
